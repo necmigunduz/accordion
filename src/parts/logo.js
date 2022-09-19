debbd89a9d45e4cffad2e8594175cf2ddfecf2a4
necmigunduz <necmigunduz@gmail.com>
2022-09-19 21:23:48 +0300

    Change logo bg color

--- src/parts/logo.js
+++ src/parts/logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='bg-slate-900 pl-[3.3em] py-4  w-[13.7em]'>
+    <div className='bg-slate-700 pl-[3.3em] py-4  w-[13.7em]'>
         <strong className='text-slate-300'>Sociality</strong>
         <span className='text-slate-400'>.io</span>
     </div>
