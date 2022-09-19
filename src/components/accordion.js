aaa8eaf600e204f3d88ba01bacbbac8366308f16
necmigunduz <necmigunduz@gmail.com>
2022-09-19 17:48:30 +0300

    Restyle leftsidenav

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -9,17 +9,17 @@ const Accordion = ({ accordionName, btnList }) => {
     setIsActive(!isActive);
   };
   const titleClassName =
-    "bg-slate-700 text-slate-300 font-bold hover:bg-slate-700 hover:text-slate-200 m-2";
+    "text-slate-300 font-bold hover:text-slate-200 m-1 pl-2";
   const bodyClassName =
-    "bg-slate-500 my-[0.1em] text-center py-1 text-slate-100 text-xs text-left hover:bg-slate-400";
+    "bg-slate-500 my-1 text-center py-1 text-slate-100 text-xs text-left hover:bg-slate-400";
   return (
     <div className="flex flex-col bg-slate-600 w-[10em] my-1 place-y-2">
-      <div className="bg-slate-700 p-2">
+      <div className="bg-slate-700 hover:bg-slate-600 p-1">
         {btnList.length > 0 ? (
           isActive ? (
-            <FontAwesomeIcon icon={solid("plus")} className="text-slate-200" onClick={handleAccordion} />
+            <FontAwesomeIcon icon={solid("plus")} className="text-slate-200 text-[8px] ml-1 mb-1" onClick={handleAccordion} />
           ) : (
-            <FontAwesomeIcon icon={solid("minus")} className="text-slate-200" onClick={handleAccordion} />
+            <FontAwesomeIcon icon={solid("minus")} className="text-slate-200 text-[8px] ml-1 mb-1" onClick={handleAccordion} />
           )
         ) : null}
         <Button
